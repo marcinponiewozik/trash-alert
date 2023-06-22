638a1414ed3546e0f1639c03dd5273f074b6905f
Marcin Poniewozik <marcin.poniewozik@gmail.com>
2023-06-22 19:33:22 +0200

    add target to repository

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,3 +1,52 @@
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (1, '9fe0b9dc-b210-11ed-afa1-0242ac120002', 1, NULL, 'LUBELSKI', 'JABŁONNA', 'LUBELSKIE', '0609062', '2024-01-01');
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (2, '9fe0b9dc-b210-11ed-afa1-0242ac120003', 1, NULL, 'LUBELSKI', 'WYSOKIE', 'LUBELSKIE', '0905646', '2024-01-01');
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (3, '9fe0b9dc-b210-11ed-afa1-0242ac120004', 1, NULL, 'LUBELSKI', 'GŁUSK', 'LUBELSKIE', '0609052', '2024-01-01');
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (4, '9fe0b9dc-b210-11ed-afa1-0242ac120005', 1, NULL, 'LUBELSKI', 'KRZCZONÓW', 'LUBELSKIE', '1064634', '2024-01-01');
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (5, '9fe0b9dc-b210-11ed-afa1-0242ac120006', 1, NULL, 'LUBELSKI', 'STRZYZEWICE', 'LUBELSKIE', '0392030', '2024-01-01');
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (6, '9fe0b9dc-b210-11ed-afa1-0242ac120007', 1, NULL, 'LUBELSKI', 'BYCHAWA', 'LUBELSKIE', '0609034', '2024-01-01');
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (7, '9fe0b9dc-b210-11ed-afa1-0242ac120008', 1, NULL, 'LUBELSKI', 'NIEMCE', 'LUBELSKIE', '0387625', '2024-01-01');
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (8, '9fe0b9dc-b210-11ed-afa1-0242ac120009', 1, NULL, 'LUBELSKI', 'JASTKÓW', 'LUBELSKIE', '0609072', '2024-01-01');
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (9, '9fe0b9dc-b210-11ed-afa1-0242ac120010', 1, NULL, 'LUBELSKI', 'KONOPNICA', 'LUBELSKIE', '0383426', '2024-01-01');
+INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (10, '9fe0b9dc-b210-11ed-afa1-0242ac120011', 1, NULL, 'LUBELSKI', 'NIEDRZWICA DUŻA', 'LUBELSKIE', '0386844', '2024-01-01');
+
+
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Chmiel Pierwszy');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Chmiel Drugi');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Czerniejów');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Czerniejów-Kolonia');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Jabłonna Druga');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Jabłonna Pierwsza');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Jabłonna-Majątek');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Piotrków Pierwszy');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Piotrkó Drugi');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Piotrków-Kolonia');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Skrzynice Drugie');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Skrzynice Pierwsze');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Skrzynice-Kolonia');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Tuszów');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Wierciszów');
+INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Wolnica');
+
+INSERT INTO public.schedule (id, uuid, version, details, end_date, file_url, start_date, unit_id) VALUES (1, '0bf2ba0d-6627-43db-8968-e20e90203193', 1, 'Właściciel gospodarstwa ma obowiązek wystawić pojemnik oraz worki z odpadami w dniu przewidzianego odbioru do godz. 7:00, a niezastosowanie się do powyższego będzie
+skutkowało nieodebraniem odpadów', '2023-08-31', 'https://cdn05.sulimo.pl/media/userfiles/jablonna.lubelskie.pl/Aktualnoci/2022/grudzien/319f3059dc26a45c056c4720323700bc.pdf', '2023-01-01', 1);
+
+INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (2, '26095688-d08c-419f-9624-ccae4de84ef7', 1, 'Czerniejów, Czerniejów Kolonia w tym linia środkowa i
+podleśna, Skrzynice Kolonia tylko linia środkowa i
+podleśna, Chmiel Kolonia tylko przy drodze CzerniejówChmiel,
+Chmiel I tylko Majdan Chmielowski, Skrzynice II tylko nr
+posesji 20 i 20A, Jabłonna Majątek tylko nr posesji nr 37,
+38, 39.', 'II', 1);
+INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (3, '0a18aa39-0caf-4bde-89d2-87aeb7fac524', 1, 'Piotrków I tylko Sachalin, Chmiel I, Chmiel II, Wolnica,
+Skrzynice I, Skrzynice II, Skrzynice Kolonia tylko przy
+drodze Chmiel-Skrzynice, Chmiel Kolonia tylko przy
+drodze Chmiel-Skrzynice, Czerniejów Kolonia tylko nr
+posesji 14.', 'III', 1);
+INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (4, '768e7de8-a227-4084-bbe6-85b9b2e08b7d', 1, 'Jabłonna I bez drogi tzw. Wesołej oraz posesji od nr 1
+do nr 5, Jabłonna II bez posesji od nr 171 do nr 175A
+oraz o nr dz. 19/2, Wierciszów.', 'IV', 1);
+INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (5, 'd34041b1-2108-4279-850b-03b64e0ce858', 1, 'Piotrków I bez Sachalina, Piotrków II, Piotrków Kolonia.', 'V', 1);
+INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (1, 'a72d80c1-a34c-4a4d-ad4e-7065aadeb6e6', 2, 'Jabłonna Majątek, Jabłonna I tylko droga tzw. Wesoła oraz', 'I', 1);
+
 INSERT INTO public.garbage_collection_date (id, uuid, version, details, trash_category, sector_id) VALUES (151, '41a82311-ae3c-4395-8f8b-1caba074b738', 1, 'Odpady segregowane', 'P', 2);
 INSERT INTO public.garbage_collection_date (id, uuid, version, details, trash_category, sector_id) VALUES (152, '60e76cf8-8d67-4f3d-8aa1-ef6c80f9deb2', 1, 'Odpady segregowane', 'ZI,N', 2);
 INSERT INTO public.garbage_collection_date (id, uuid, version, details, trash_category, sector_id) VALUES (154, '16dcba8a-31c7-40be-9a78-d39190e6952b', 1, 'Odpady segregowane', 'P', 3);
@@ -179,73 +228,3 @@ INSERT INTO public.garbage_collection_date_dates (garbage_collection_date_id, da
 INSERT INTO public.garbage_collection_date_dates (garbage_collection_date_id, date) VALUES (165, '2023-07-09');
 INSERT INTO public.garbage_collection_date_dates (garbage_collection_date_id, date) VALUES (165, '2023-04-02');
 INSERT INTO public.garbage_collection_date_dates (garbage_collection_date_id, date) VALUES (165, '2023-06-02');
-
-
---
--- TOC entry 3357 (class 0 OID 16436)
--- Dependencies: 220
--- Data for Name: schedule; Type: TABLE DATA; Schema: public; Owner: -
---
-
-INSERT INTO public.schedule (id, uuid, version, details, end_date, file_url, start_date, unit_id) VALUES (1, '0bf2ba0d-6627-43db-8968-e20e90203193', 1, 'Właściciel gospodarstwa ma obowiązek wystawić pojemnik oraz worki z odpadami w dniu przewidzianego odbioru do godz. 7:00, a niezastosowanie się do powyższego będzie
-skutkowało nieodebraniem odpadów', '2023-08-31', 'https://cdn05.sulimo.pl/media/userfiles/jablonna.lubelskie.pl/Aktualnoci/2022/grudzien/319f3059dc26a45c056c4720323700bc.pdf', '2023-01-01', 1);
-
-
---
--- TOC entry 3351 (class 0 OID 16413)
--- Dependencies: 214
--- Data for Name: sector; Type: TABLE DATA; Schema: public; Owner: -
---
-
-INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (2, '26095688-d08c-419f-9624-ccae4de84ef7', 1, 'Czerniejów, Czerniejów Kolonia w tym linia środkowa i
-podleśna, Skrzynice Kolonia tylko linia środkowa i
-podleśna, Chmiel Kolonia tylko przy drodze CzerniejówChmiel,
-Chmiel I tylko Majdan Chmielowski, Skrzynice II tylko nr
-posesji 20 i 20A, Jabłonna Majątek tylko nr posesji nr 37,
-38, 39.', 'II', 1);
-INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (3, '0a18aa39-0caf-4bde-89d2-87aeb7fac524', 1, 'Piotrków I tylko Sachalin, Chmiel I, Chmiel II, Wolnica,
-Skrzynice I, Skrzynice II, Skrzynice Kolonia tylko przy
-drodze Chmiel-Skrzynice, Chmiel Kolonia tylko przy
-drodze Chmiel-Skrzynice, Czerniejów Kolonia tylko nr
-posesji 14.', 'III', 1);
-INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (4, '768e7de8-a227-4084-bbe6-85b9b2e08b7d', 1, 'Jabłonna I bez drogi tzw. Wesołej oraz posesji od nr 1
-do nr 5, Jabłonna II bez posesji od nr 171 do nr 175A
-oraz o nr dz. 19/2, Wierciszów.', 'IV', 1);
-INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (5, 'd34041b1-2108-4279-850b-03b64e0ce858', 1, 'Piotrków I bez Sachalina, Piotrków II, Piotrków Kolonia.', 'V', 1);
-INSERT INTO public.sector (id, uuid, version, localities, row_symbol, schedule_id) VALUES (1, 'a72d80c1-a34c-4a4d-ad4e-7065aadeb6e6', 2, 'Jabłonna Majątek, Jabłonna I tylko droga tzw. Wesoła oraz', 'I', 1);
-
-
---
--- TOC entry 3352 (class 0 OID 16420)
--- Dependencies: 215
--- Data for Name: unit; Type: TABLE DATA; Schema: public; Owner: -
---
-
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (1, '9fe0b9dc-b210-11ed-afa1-0242ac120002', 1, NULL, 'LUBELSKI', 'JABŁONNA', 'LUBELSKIE', '0609062', '2024-01-01');
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (2, '9fe0b9dc-b210-11ed-afa1-0242ac120003', 1, NULL, 'LUBELSKI', 'WYSOKIE', 'LUBELSKIE', '0905646', '2024-01-01');
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (3, '9fe0b9dc-b210-11ed-afa1-0242ac120004', 1, NULL, 'LUBELSKI', 'GŁUSK', 'LUBELSKIE', '0609052', '2024-01-01');
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (4, '9fe0b9dc-b210-11ed-afa1-0242ac120005', 1, NULL, 'LUBELSKI', 'KRZCZONÓW', 'LUBELSKIE', '1064634', '2024-01-01');
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (5, '9fe0b9dc-b210-11ed-afa1-0242ac120006', 1, NULL, 'LUBELSKI', 'STRZYZEWICE', 'LUBELSKIE', '0392030', '2024-01-01');
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (6, '9fe0b9dc-b210-11ed-afa1-0242ac120007', 1, NULL, 'LUBELSKI', 'BYCHAWA', 'LUBELSKIE', '0609034', '2024-01-01');
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (7, '9fe0b9dc-b210-11ed-afa1-0242ac120008', 1, NULL, 'LUBELSKI', 'NIEMCE', 'LUBELSKIE', '0387625', '2024-01-01');
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (8, '9fe0b9dc-b210-11ed-afa1-0242ac120009', 1, NULL, 'LUBELSKI', 'JASTKÓW', 'LUBELSKIE', '0609072', '2024-01-01');
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (9, '9fe0b9dc-b210-11ed-afa1-0242ac120010', 1, NULL, 'LUBELSKI', 'KONOPNICA', 'LUBELSKIE', '0383426', '2024-01-01');
-INSERT INTO public.unit (id, uuid, version, details, district, name, province, unique_code, license_end_date) VALUES (10, '9fe0b9dc-b210-11ed-afa1-0242ac120011', 1, NULL, 'LUBELSKI', 'NIEDRZWICA DUŻA', 'LUBELSKIE', '0386844', '2024-01-01');
-
-
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Chmiel Pierwszy');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Chmiel Drugi');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Czerniejów');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Czerniejów-Kolonia');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Jabłonna Druga');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Jabłonna Pierwsza');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Jabłonna-Majątek');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Piotrków Pierwszy');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Piotrkó Drugi');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Piotrków-Kolonia');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Skrzynice Drugie');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Skrzynice Pierwsze');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Skrzynice-Kolonia');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Tuszów');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Wierciszów');
-INSERT INTO public.unit_locality (unit_id, locality) VALUES (1, 'Wolnica');
